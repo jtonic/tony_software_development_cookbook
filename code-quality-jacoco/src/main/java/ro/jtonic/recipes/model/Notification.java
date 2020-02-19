cb2bd6808d77faea263929c491400eb105b3fea5
jtonic <antonel.pazargic@gmail.com>
2020-02-19 06:28:16 +0200

    step 4: configure lombok in pom.xml. create lombok.config to configure the `lombok.addLombokGeneratedAnnotation = true` so that jacoco ignored the lombok generated classes

--- code-quality-jacoco/src/main/java/ro/jtonic/recipes/model/Notification.java
+++ code-quality-jacoco/src/main/java/ro/jtonic/recipes/model/Notification.java
@@ -1,81 +1,22 @@
 package ro.jtonic.recipes.model;
 
-import java.util.Objects;
-import java.util.StringJoiner;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode
 public class Notification {
 
   private String id;
   private String message;
   private String recipient;
   private String type;
-
-  public Notification(String id, String message, String recipient, String type) {
-    this.id = id;
-    this.message = message;
-    this.recipient = recipient;
-    this.type = type;
-  }
-
-  public Notification() {
-  }
-
-  public String getId() {
-    return id;
-  }
-
-  public void setId(String id) {
-    this.id = id;
-  }
-
-  public String getMessage() {
-    return message;
-  }
-
-  public void setMessage(String message) {
-    this.message = message;
-  }
-
-  public String getRecipient() {
-    return recipient;
-  }
-
-  public void setRecipient(String recipient) {
-    this.recipient = recipient;
-  }
-
-  public String getType() {
-    return type;
-  }
-
-  public void setType(String type) {
-    this.type = type;
-  }
-
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) {
-      return true;
-    }
-    if (o == null || getClass() != o.getClass()) {
-      return false;
-    }
-    Notification that = (Notification) o;
-    return id.equals(that.id);
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(id);
-  }
-
-  @Override
-  public String toString() {
-    return new StringJoiner(", ", Notification.class.getSimpleName() + "[", "]")
-        .add("id='" + id + "'")
-        .add("message='" + message + "'")
-        .add("recipient='" + recipient + "'")
-        .add("type='" + type + "'")
-        .toString();
-  }
 }
