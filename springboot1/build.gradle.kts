5564f159fec6b18f7de4e49e673396e215669b01
jtonic <antonel.pazargic@gmail.com>
2020-03-20 21:00:03 +0200

    [sb 1, sc 4] configure spring web.

--- springboot1/build.gradle.kts
+++ springboot1/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
   java
+  application
 }
 
 group = "ro.jtonic.springboot1"
@@ -11,7 +12,7 @@ repositories {
 
 dependencies {
 
-  implementation("org.springframework.boot:spring-boot-starter:$springBootVersion")
+  implementation("org.springframework.boot:spring-boot-starter-web:$springBootVersion")
 
   testImplementation("org.springframework.boot:spring-boot-starter-test:$springBootVersion")
   testImplementation("junit:junit:$junitVersion")
@@ -19,4 +20,8 @@ dependencies {
 
 configure<JavaPluginConvention> {
   sourceCompatibility = JavaVersion.VERSION_1_8
+}
+
+application {
+  mainClassName = "ro.jtonic.handson.springboot1.MainApp"
 }
