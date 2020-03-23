7d639bcd53006447d1f660fcc48c0921543e71c4
jtonic <antonel.pazargic@gmail.com>
2020-03-23 21:18:29 +0200

    update kotlin lib and coroutines versions

--- kotlin-concurrency/buildSrc/src/main/kotlin/Versions.kt
+++ kotlin-concurrency/buildSrc/src/main/kotlin/Versions.kt
@@ -2,8 +2,8 @@ const val appVersion = "0.1.0-SNAPSHOT"
 
 const val javaVersion = "1.8"
 
-const val kotlinVersion = "1.3.70"
-const val kotlinCoroutinesVersion = "1.3.3-1.3.70-eap-42"
+const val kotlinVersion = "1.3.71"
+const val kotlinCoroutinesVersion = "1.3.5"
 
 const val kotlinTestVersion = "3.4.2"
 
