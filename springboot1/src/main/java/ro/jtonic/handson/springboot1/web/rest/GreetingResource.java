30e6c420466a9fef4c5136e206844ccb794de2ed
jtonic <antonel.pazargic@gmail.com>
2020-03-25 07:04:42 +0200

    [springboot1][actos] Fix akka.actor.InvalidActorNameException: actor name [GreetingActor] is not unique!

--- springboot1/src/main/java/ro/jtonic/handson/springboot1/web/rest/GreetingResource.java
+++ springboot1/src/main/java/ro/jtonic/handson/springboot1/web/rest/GreetingResource.java
@@ -3,6 +3,7 @@ package ro.jtonic.handson.springboot1.web.rest;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
+import java.util.UUID;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -26,11 +27,17 @@ public class GreetingResource {
   @ResponseStatus(HttpStatus.ACCEPTED)
   public void greeting(@RequestParam("name") String name) {
 
+    //fixme jtonic: This dummy solution (singleton actor or what? to handle?
+    // akka.actor.InvalidActorNameException: actor name [GreetingActor] is not unique!
+
     final SpringExt springExt = SpringExtension.SPRING_EXTENSION_PROVIDER.get(actorSystem);
 
     final Props actorProps = springExt.props(GreetingActor.NAME);
 
-    final ActorRef greetingActorRef = actorSystem.actorOf(actorProps, GreetingActor.NAME);
+    final String actorName = GreetingActor.NAME + UUID.randomUUID().toString();
+
+    final ActorRef greetingActorRef = actorSystem.actorOf(actorProps,
+        actorName);
 
     greetingActorRef.tell(new Greet(name), ActorRef.noSender());
   }
