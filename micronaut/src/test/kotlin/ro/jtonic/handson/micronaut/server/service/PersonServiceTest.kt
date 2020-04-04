d7ea6210b459cafa2ae0616ebb43f04ba8168e97
jtonic <antonel.pazargic@gmail.com>
2020-04-04 16:48:10 +0300

    [micronaut] Test simplification with @MicronautTest

--- micronaut/src/test/kotlin/ro/jtonic/handson/micronaut/server/service/PersonServiceTest.kt
+++ micronaut/src/test/kotlin/ro/jtonic/handson/micronaut/server/service/PersonServiceTest.kt
@@ -1,31 +1,24 @@
 package ro.jtonic.handson.micronaut.server.service
 
-import io.micronaut.context.BeanContext
+import io.micronaut.test.annotation.MicronautTest
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
+import javax.inject.Inject
 
+@MicronautTest
 class PersonServiceTest {
+
+  @Inject
+  private lateinit var personService: PersonService
 
   @Test
   @DisplayName("find all persons")
   fun findAll() {
-    val personService = ctx.getBean(PersonService::class.java)
+
     val persons = personService.findAll()
 
     assertThat(persons)
       .hasSize(2)
-  }
-
-  companion object {
-
-    private lateinit var ctx: BeanContext
-
-    @BeforeAll
-    @JvmStatic
-    fun beforeAll() {
-      ctx = BeanContext.run()
-    }
   }
 }
