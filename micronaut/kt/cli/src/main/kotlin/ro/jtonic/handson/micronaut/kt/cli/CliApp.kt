c92fd996458bb5fd7063ca69afe56ed92006fd04
jtonic <antonel.pazargic@gmail.com>
2020-04-13 07:38:00 +0300

    [micronaut][IoC] using mn ApplicationContext

--- micronaut/kt/cli/src/main/kotlin/ro/jtonic/handson/micronaut/kt/cli/CliApp.kt
+++ micronaut/kt/cli/src/main/kotlin/ro/jtonic/handson/micronaut/kt/cli/CliApp.kt
@@ -1,16 +1,15 @@
 package ro.jtonic.handson.micronaut.kt.cli
 
-import io.micronaut.context.BeanContext
+import io.micronaut.context.ApplicationContext
 import ro.jtonic.handson.micronaut.kt.cli.service.PersonService
 
 object CliApp {
 
   @JvmStatic
   fun main(args: Array<String>) {
-    val ctx = BeanContext.run()
-    val personService = ctx.getBean(PersonService::class.java)
-    val allPersons = personService.findAll()
-
-    allPersons.forEach(::println)
+      ApplicationContext.run().use {
+      val personService = it.getBean(PersonService::class.java)
+      personService.findAll().forEach(::println)
+    }
   }
 }
