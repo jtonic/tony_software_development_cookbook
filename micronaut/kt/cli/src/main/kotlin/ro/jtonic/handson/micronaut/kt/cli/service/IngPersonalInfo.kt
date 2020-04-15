e17f0039dbff91d455f6d21bccd7df8dd245d2eb
jtonic <antonel.pazargic@gmail.com>
2020-04-15 08:08:22 +0300

    [micronaut][IoC] beans qualifiers - part 2: @Named

--- micronaut/kt/cli/src/main/kotlin/ro/jtonic/handson/micronaut/kt/cli/service/IngPersonalInfo.kt
+++ micronaut/kt/cli/src/main/kotlin/ro/jtonic/handson/micronaut/kt/cli/service/IngPersonalInfo.kt
@@ -1,10 +1,11 @@
 package ro.jtonic.handson.micronaut.kt.cli.service
 
 import ro.jtonic.handson.micronaut.kt.cli.model.Employee
+import javax.inject.Named
 import javax.inject.Singleton
 
 @Singleton
-class IngPersonalInfo (val employeeService: EmployeeService) {
+class IngPersonalInfo (@Named("ING") private val employeeService: EmployeeService) {
 
   fun getInfo(personName: String): Employee = employeeService.getByName(personName)
 }
--- micronaut/kt/cli/src/main/kotlin/ro/jtonic/handson/micronaut/kt/cli/service/IntEmployeeService.kt
+++ micronaut/kt/cli/src/main/kotlin/ro/jtonic/handson/micronaut/kt/cli/service/IntEmployeeService.kt
@@ -1,9 +1,11 @@
 package ro.jtonic.handson.micronaut.kt.cli.service
 
 import ro.jtonic.handson.micronaut.kt.cli.model.Employee
+import javax.inject.Named
 import javax.inject.Singleton
 
 @Singleton
+@Named("ING")
 class IntEmployeeService : EmployeeService {
 
   override fun getByName(name: String)= Employee(name, "ING")
