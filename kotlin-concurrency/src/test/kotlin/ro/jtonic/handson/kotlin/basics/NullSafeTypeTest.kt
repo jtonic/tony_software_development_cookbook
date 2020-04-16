642e0f26870fb876956f7295720c5fd328b77816
jtonic <antonel.pazargic@gmail.com>
2020-04-16 06:55:23 +0300

    [presentation][kt basics] null safety - elvis operator

--- kotlin-concurrency/src/test/kotlin/ro/jtonic/handson/kotlin/basics/NullSafeTypeTest.kt
+++ kotlin-concurrency/src/test/kotlin/ro/jtonic/handson/kotlin/basics/NullSafeTypeTest.kt
@@ -1,10 +1,17 @@
 package ro.jtonic.handson.kotlin.basics
 
+import io.kotest.matchers.shouldBe
 import io.kotlintest.specs.StringSpec
 
 class NullSafeTypeTest : StringSpec() {
 
   init {
+    "test elvis operator" {
+      var address: String? = null
+      address ?: "unknown address" shouldBe "unknown address"
 
+      address = "Kotlin Island"
+      address shouldBe "Kotlin Island"
+    }
   }
 }
