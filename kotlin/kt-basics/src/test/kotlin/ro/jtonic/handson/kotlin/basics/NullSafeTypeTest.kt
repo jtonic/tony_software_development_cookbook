f6240e22b4d192ead921439ca43ded04fc6fb44c
jtonic <antonel.pazargic@gmail.com>
2020-04-17 08:52:38 +0300

    [presentation][kt basics][null safety] safe call operator

--- kotlin/kt-basics/src/test/kotlin/ro/jtonic/handson/kotlin/basics/NullSafeTypeTest.kt
+++ kotlin/kt-basics/src/test/kotlin/ro/jtonic/handson/kotlin/basics/NullSafeTypeTest.kt
@@ -1,10 +1,20 @@
 package ro.jtonic.handson.kotlin.basics
 
+import io.kotest.matchers.shouldBe
 import io.kotlintest.specs.StringSpec
 
 class NullSafeTypeTest : StringSpec() {
 
   init {
 
+    "test safe call operator" {
+      data class User(val name: String, val address: String)
+
+      var user: User? = null
+      user?.address shouldBe null
+
+      user = User("Tony", "Mihai Bravu")
+      user?.address shouldBe "Mihai Bravu"
+    }
   }
 }
