34609741ccca1e6bd5647f6eb802c5981df3aa87
jtonic <antonel.pazargic@gmail.com>
2020-04-17 08:49:53 +0300

    [ignore] revert changes

--- kotlin/kt-basics/src/test/kotlin/ro/jtonic/handson/kotlin/basics/NullSafeTypeTest.kt
+++ kotlin/kt-basics/src/test/kotlin/ro/jtonic/handson/kotlin/basics/NullSafeTypeTest.kt
@@ -1,28 +1,10 @@
 package ro.jtonic.handson.kotlin.basics
 
-import io.kotest.matchers.shouldBe
 import io.kotlintest.specs.StringSpec
 
 class NullSafeTypeTest : StringSpec() {
 
   init {
 
-    "test safe call operator" {
-      data class User(val name: String, val address: String)
-
-      var user: User? = null
-      user?.address shouldBe null
-
-      user = User("Tony", "Mihai Bravu")
-      user?.address shouldBe "Mihai Bravu"
-    }
-
-    "test elvis operator" {
-      var address: String? = null
-      address ?: "unknown address" shouldBe "unknown address"
-
-      address = "Kotlin Island"
-      address shouldBe "Kotlin Island"
-    }
   }
 }
