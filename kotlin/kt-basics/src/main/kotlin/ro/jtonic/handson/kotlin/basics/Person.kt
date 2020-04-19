c0fab8fd23cc072920541d2920b8f5e80e24165b
jtonic <antonel.pazargic@gmail.com>
2020-04-19 07:57:04 +0300

    [presentation][kt basics][idea] convert java to kotlin

--- kotlin/kt-basics/src/main/kotlin/ro/jtonic/handson/kotlin/basics/Person.kt
+++ kotlin/kt-basics/src/main/kotlin/ro/jtonic/handson/kotlin/basics/Person.kt
@@ -1,28 +1,3 @@
-package ro.jtonic.handson.kotlin.basics;
+package ro.jtonic.handson.kotlin.basics
 
-public class Person {
-
-  private String name;
-  private int age;
-
-  public Person(String name, int age) {
-    this.name = name;
-    this.age = age;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public int getAge() {
-    return age;
-  }
-
-  public void setAge(int age) {
-    this.age = age;
-  }
-}
+class Person(var name: String, var age: Int) 
