af09d87f1302d7c4c6f4cac5b0cf99b1cff4a1b5
jtonic <antonel.pazargic@gmail.com>
2020-04-22 07:00:04 +0300

    [presentation][micronaut][kt-cli] use log4j to write to console. commented out the trace micronaut log

--- presentations/surveys-mn-kt-cli/src/main/kotlin/com/nstanciu/tutorials/mn/surveys/SurveysMainCommand.kt
+++ presentations/surveys-mn-kt-cli/src/main/kotlin/com/nstanciu/tutorials/mn/surveys/SurveysMainCommand.kt
@@ -1,6 +1,8 @@
 package com.nstanciu.tutorials.mn.surveys
 
 import io.micronaut.configuration.picocli.PicocliRunner
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import picocli.CommandLine.Command
 import picocli.CommandLine.Option
 import javax.inject.Inject
@@ -18,7 +20,7 @@ class SurveysMainCommand : Runnable {
     @Option(names = ["-c", "--conf"], description = ["Print configuration"])
     private var conf : Boolean = false
 
-    @Option(names = ["-a", "--all"], description = ["Print all surveys"])
+    @Option(names = ["-a", "--all"], description = ["Print all surveys. This is the default switch"])
     private var allSurveys : Boolean = false
 
     override fun run() {
@@ -26,13 +28,19 @@ class SurveysMainCommand : Runnable {
           conf -> println(this.confBean.getConf())
           allSurveys -> {
             val allSurveys = this.client.getAllSurveys()
-            println(allSurveys)
+            LOG.info(allSurveys.toString())
           }
           else -> {
             val allSurveys = this.client.getAllSurveys()
-            println(allSurveys)
+            LOG.info(allSurveys.toString())
           }
         }
+    }
+
+    companion object {
+
+      @JvmField
+      val LOG: Logger = LoggerFactory.getLogger(SurveysMainCommand::class.java)
     }
 }
 
