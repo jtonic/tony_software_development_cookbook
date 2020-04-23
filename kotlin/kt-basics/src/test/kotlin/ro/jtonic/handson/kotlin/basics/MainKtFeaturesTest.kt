1d76432b5c381260f1ec98a911c0576baa80242b
jtonic <antonel.pazargic@gmail.com>
2020-04-23 06:10:27 +0300

    [presentation][kt basics] Pattern matching (when)

---
+++ kotlin/kt-basics/src/test/kotlin/ro/jtonic/handson/kotlin/basics/MainKtFeaturesTest.kt
@@ -0,0 +1,34 @@
+package ro.jtonic.handson.kotlin.basics
+
+import io.kotest.matchers.shouldBe
+import io.kotlintest.specs.StringSpec
+
+class MainKtFeaturesTest : StringSpec() {
+
+  init {
+
+    "test pattern matching with when (1)" {
+
+        val no = 7
+
+        val day: String = when(no) {
+          6, 7 -> "weekend"
+          else -> "week day"
+        }
+
+      day shouldBe "weekend"
+    }
+
+    "test pattern matching with when (2)" {
+
+      val person = Person("Tony", 18) as Any
+
+      val personInfo: String = when(person) {
+        is String -> person
+        is Person -> person.name  //smart cast
+        else -> person.toString()
+      }
+      personInfo shouldBe "Tony"
+    }
+  }
+}
