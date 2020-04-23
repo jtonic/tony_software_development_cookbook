81e412c195aaaf27bf4f4565797d53a375b5ab21
jtonic <antonel.pazargic@gmail.com>
2020-04-23 08:06:06 +0300

    [ignore] refactor

--- kotlin/kt-basics/src/test/kotlin/ro/jtonic/handson/kotlin/basics/Model.kt
+++ kotlin/kt-basics/src/test/kotlin/ro/jtonic/handson/kotlin/basics/Model.kt
@@ -4,4 +4,4 @@ data class User(val name: String, val address: String)
 
 data class Person(val name: String, val age: Int)
 
-data class Employee(val firstName: String, val lastName: String, val age: Int = 18)
+data class Employee(val firstName: String, val lastName: String, val age: Int = 18, val salary: Int = 1500)
