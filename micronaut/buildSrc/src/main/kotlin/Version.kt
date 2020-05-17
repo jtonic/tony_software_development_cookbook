6782a16f1ecfb7d0bcc048c26bd8f660372286c0
jtonic <antonel.pazargic@gmail.com>
2020-05-17 21:03:47 +0300

    [ignore][micronaut] bump micronaut version

--- micronaut/buildSrc/src/main/kotlin/Version.kt
+++ micronaut/buildSrc/src/main/kotlin/Version.kt
@@ -2,7 +2,7 @@ const val jvmVersion = "1.8"
 
 const val kotlinVersion = "1.3.72"
 
-const val micronautVersion = "1.3.3"
+const val micronautVersion = "1.3.4"
 const val mockitoVersion = "2.22.0"
 const val jacksonKtModuleVersion = "2.9.8"
 const val logbackVersion = "1.2.3"
