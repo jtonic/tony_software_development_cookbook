0a1a29adb582b3192c7c92c674736ff49014f59d
jtonic <antonel.pazargic@gmail.com>
2020-08-23 11:17:11 +0300

    [sb23] update the kt version to 1.4 and gradle version to 6.6.

--- handson-sb23/buildSrc/src/main/kotlin/Project.kt
+++ handson-sb23/buildSrc/src/main/kotlin/Project.kt
@@ -1,6 +1,6 @@
 object Versions {
 
-  const val kotlin = "1.3.72"
-  const val springBoot = "2.3.0.RELEASE"
+  const val kotlin = "1.4.0"
+  const val springBoot = "2.3.3.RELEASE"
   const val springDependencyManagement = "1.0.9.RELEASE"
 }
